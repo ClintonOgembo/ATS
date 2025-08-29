d22d1b17dd23acbbf6c08edfbdb92f4a1de9901a
ClintonOgembo <clintonogembo70@gmail.com>
2025-08-29 14:27:46 +0300

    client.cs db

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,7 +1,22 @@
-﻿namespace AttainTaskSupportSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttainTaskSupportSystem.Models
 {
     public class Client
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string role { get; set; } = "staff";
     }
 }
